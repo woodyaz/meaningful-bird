82ee8cae59d24ab8a2ba1714aa57fa60df5ab114
Woody Hansen <woodyaz@users.noreply.github.com>
2018-03-04 20:19:16 -0800

    Use @mention instead of comments.

--- src/handler.js
+++ src/handler.js
@@ -2,27 +2,34 @@ import Database from './database'
 const database = new Database()
 
 export default class Hander {
-  constructor () {
-    this.keyword = '+' + (process.env.KEYWORD || 'mb')
-  }
-
-  validateKeyword (data) {
-    const change = data.entry[0].changes[0]
-    if (change.value.message === this.keyword) {
-      this.router(change)
-    }
-  }
-
-  router (change) {
+  post (body) {
+    const change = body.entry[0].changes[0]
     switch (change.field) {
-      case 'comments': {
-        if (change.value.verb === 'add') {
-          database.save(change.value.post_id, change.value)
-          console.log(`Following ${change.value.post_id}`)
-          return true
-        }
+      case 'mention': {
+        this.mention(change)
         break
       }
+      case 'posts': {
+        if (change.value.verb === 'edit') {
+          this.edit(change)
+        }
+      }
     }
+  }
+  mention (change) {
+    // check database
+    const postId = change.value.post_id
+    if (database.findById(postId)) {
+            
+    }
+    // if not there, save it
+    // call talk
+    // else
+    // save it
+    // call talk 
+  }
+
+  edit () {
+
   }
 }
